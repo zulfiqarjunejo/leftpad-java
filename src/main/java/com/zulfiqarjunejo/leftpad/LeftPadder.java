70e6ebe6f98a3d502ef38cc68f05beea3697d383
Falak Zulfiqar <memonfalaksaleh@gmail.com>
2024-08-25 14:06:36 +0200

    refactor: refactor the code to make it concise

--- src/main/java/com/zulfiqarjunejo/leftpad/LeftPadder.java
+++ src/main/java/com/zulfiqarjunejo/leftpad/LeftPadder.java
@@ -6,19 +6,17 @@ public class LeftPadder {
     }
 
     public String pad(String text, int len, char ch) {
-        var textLength = text.length();
-        if (textLength >= len) {
+        if (text.length() >= len) {
             return text;
         }
 
         var result = new char[len];
-        
-        var j = textLength - 1;
-        for (int i=len-1; i>=0; i--) {
-            if (i<textLength && j<0) {
+
+        for (int i=len-1, j=text.length()-1; i>=0; i--, j--) {
+            if (j<0) {
                 result[i] = ch;
             } else {
-                result[i] = text.charAt(j--);
+                result[i] = text.charAt(j);
             }
         }
 
