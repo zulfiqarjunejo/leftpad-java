b9b4682fb68bbc2dbc50aed7c487fedbf03ab4d3
Falak Zulfiqar <memonfalaksaleh@gmail.com>
2024-08-25 13:22:23 +0200

    fix: replace bitwise AND operator with logical AND operator

--- src/main/java/com/zulfiqarjunejo/leftpad/App.java
+++
@@ -1,9 +0,0 @@
-package com.zulfiqarjunejo.leftpad;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
--- src/main/java/com/zulfiqarjunejo/leftpad/LeftPadder.java
+++ src/main/java/com/zulfiqarjunejo/leftpad/LeftPadder.java
@@ -15,7 +15,7 @@ public class LeftPadder {
         
         var j = textLength - 1;
         for (int i=len-1; i>=0; i--) {
-            if (i<textLength & j<0) {
+            if (i<textLength && j<0) {
                 result[i] = ch;
             } else {
                 result[i] = text.charAt(j--);
